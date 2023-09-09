eb696eaffce3289ee892b7b8ddbc786f8cbcf73e
Cenrique <cyn_thia84@hotmail.com>
2023-09-09 09:02:51 -0600

    Inicio proyecto de ecommerce

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo de GIT");
+console.log("Hola mundo de Git");
+
+
+let nombre = "Berni";
+let provincia = "Mendoza";
+let pais = "Argentina";
+let fecha = 2023;
+
+
+console.log("Buenas, mi nombre es " + nombre + " y vivo en la provincia de " + provincia + " que se encuentra en " + pais );
