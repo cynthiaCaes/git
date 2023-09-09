d46e0d507b62c670614aa77cd80e5ec0022c8214
cynthiaCaes <143891618+cynthiaCaes@users.noreply.github.com>
2023-09-09 09:58:58 -0600

    Creamos una funcion2

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ let pais = "Argentina";
 let fecha = 2023;
 
 
+console.log('================================================');
 console.log("Buenas, mi nombre es " + nombre + " y vivo en la provincia de " + provincia + " que se encuentra en " + pais );
+
+console.log('================================================');
+
+function sumar() {
+    let numeroUno = 55000;
+    let numeroDos = 89000;
+
+    let resultado = numeroUno + numeroDos;
+
+    console.log('================================================');
+    console.log('El resulado de la suma es: ${resultado}');
+    console.log('================================================');
+    console.log("El resulado de la suma es: " + resultado);
+    console.log('================================================');
+    console.log('El resulado de la suma es: ' + resultado);
+    console.log('================================================');
+}
+sumar();
+
+
+
